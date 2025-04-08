fc2a70f9ef1bb24088ac3afb1a456939b31eb160
CodyCodesk <s.krstic55@gmail.com>
2025-04-08 22:56:58 +0200

    Analytics added

--- kc-performance/src/app/layout.tsx
+++ kc-performance/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import SchemaScript from "@/components/SchemaScript";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
